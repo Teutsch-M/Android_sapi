046b7f173be5298c547a4abcad57c2bbc12f4d80
Teutsch-M <mlteutsch@gmail.com>
2021-10-03 20:54:51 +0300

    II Labor

--- KotlinBasics/Lab2/src/dates/Date.kt
+++ KotlinBasics/Lab2/src/dates/Date.kt
@@ -1,5 +1,52 @@
 package dates
 
-data class Date(){
+
+import java.util.Calendar
+
+val now = Calendar.getInstance()
+
+data class Date(val year: Int = now.get(Calendar.YEAR) , val month: Int = now.get(Calendar.MONTH)+1, val day: Int = now.get(Calendar.DAY_OF_MONTH)): Comparable<Date>{
+
+    override fun compareTo(other: Date): Int {
+        return comp.compare(this,other)
+    }
+
+    companion object{
+        val comp = Comparator.comparingInt<Date> {it.year}. thenComparingInt { it.month }. thenComparingInt { it.day }
+    }
+
+}
+
+fun Date.isLeap(): Boolean{
+    if(this.year % 4 == 0) {
+        if (this.year % 100 == 0) {
+            if (this.year % 400 == 0)
+                return true
+            return false
+        }
+        return true
+    }
+    return false
+}
+
+
+fun Date.isValid(): Boolean {
+
+ if(this.year <= 0 || this.month <= 0 || this.month > 12 || this.day <= 0 )
+     return false
+
+if (this.month == 2)
+    if (this.isLeap() && this.day > 29 || (!this.isLeap() && this.day > 28))
+        return false
+
+ if(this.month < 8){
+     if(this.month % 2 != 0 && this.day > 31 || (this.month % 2 == 0 && this.day > 30))
+             return false
+ }
+ else if(this.month % 2 == 0 && this.day > 31 || (this.month % 2 != 0 && this.day > 30))
+     return false
+
+
+ return true
 
 }
--- KotlinBasics/Lab2/src/dates/Main.kt
+++ KotlinBasics/Lab2/src/dates/Main.kt
@@ -1,6 +1,33 @@
 package dates
 
+import java.util.Random
+
 fun main(){
 
+    var s = 0
+    val list = mutableListOf<Date>()
+    val comp = Comparator.comparingInt<Date> {it.year}. thenComparingInt { it.month }. thenComparingInt { it.day }
+
+    while(s<10){
+       val datum = Date(Random().nextInt(-3000,3000),Random().nextInt(-12,12),Random().nextInt(-31,31))
+        if(datum.isValid()) {
+            list.add(s, datum)
+            s++
+        }
+        else
+            println(datum)
+
+    }
+    println("Valid: ")
+    list.forEach{ println(it)}
+
+
+    list.sortWith(comp)
+    println("Ordered: ")
+    list.forEach{ println(it)}
+
+    list.reverse()
+    println("Reversed: ")
+    list.forEach { println(it) }
 
 }
